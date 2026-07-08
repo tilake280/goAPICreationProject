f1b29a25cdaf92f6826eb9868fdc1ffa4d6ae782
Ethan Tilak <ethantilak@gmail.com>
2026-07-07 20:27:37 -0400

    now provides error using slog package to make it clear when error has occured

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"os"
 )
 
@@ -14,8 +14,13 @@ func main() {
 		config: cfg,
 	}
 
+	// Logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	slog.SetDefault(logger)
+
 	if err := api.run(api.mount()); err != nil {
-		log.Println("server has failed to start, err: %s", err)
+		slog.Error("server failed to start", "error", err)
 		os.Exit(1)
 	}
 }
